0400235d5b9113d62bb0d1f1f304a3579cf20f01
mahim008 <94555565+mahim008@users.noreply.github.com>
2022-06-13 00:43:38 +0600

    Add files via upload

---
+++ All_check.py
@@ -0,0 +1,46 @@
+import re
+
+email = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+master_card = '^5[1-4][0-9]{12}(?:[0-9]{3})?$'
+visa_card = '^4[0-9]{12}(?:[0-9]{3})?$'
+number = '[+-]?[0-9]+\.[0-9]+$'
+
+def check_email(s):
+
+    if(re.search(email,s)):
+        print("Valid Email\n")
+    else:
+        print("Invalid Email\n")
+
+def check_card(c):
+
+    if(re.search(master_card,c)):
+        print("Mastercard.\n")
+    elif(re.search(visa_card, c)):
+        print("VISA Card.\n")
+    else:
+        print("Invalid Card Number.\n")
+
+def check_number(n):
+    if(re.search(number,n)):
+        print("Floating Number.\n")
+    else:
+        print("Not floating number.\n")
+
+while(True):
+    print("Menu\n1. Card\n2. Number\n3. Email\n4. Exit")
+    command = input(">> ")
+
+    if command=='1':
+        c = input("\nEnter card number to check : ")
+        check_card(c)
+    elif command=='2':
+        n = input("\nEnter number to check : ")
+        check_number(n)
+    elif command=='3':
+        s = input("\nEnter mail to check : ")
+        check_email(s)
+    elif command=='4':
+        break
+    else:
+        print("Error occured. Try again.")
---
+++ Check_vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void check_vowel(char s)
+{
+    if(s=='a'|'A'|'e'|'E'|'i'|'I'|'o'|'O'|'u'|'U')
+        printf("%c is vowel.",s);
+    else
+        printf("%c is consonent.",s);
+}
+
+main()
+{
+    char c;
+    printf("Enter a character to check : ");
+    scanf("%c",&c);
+    check_vowel(c);
+    return 0;
+}
